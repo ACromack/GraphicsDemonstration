dc37c7a549e03062d6ff8d4b477fbedf1bb8c1a8
Ashley Cromack <Cromazone@live.co.uk>
2018-01-25 11:51:03 +0000

    Setup
    Add the work for the first workshop to the repository

---
+++ GraphicsCode/src/main.h
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+// // GLEW - OpenGL Extension Wrangler - http://glew.sourceforge.net/
+// // NOTE: include before SDL.h
+#include <GL/glew.h>
+#include "windows.h"
+
+// SDL - Simple DirectMedia Layer - https://www.libsdl.org/
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "SDL_ttf.h"
+
+// // - OpenGL Mathematics - https://glm.g-truc.net/
+#define GLM_FORCE_RADIANS // force glm to use radians
+// // NOTE: must do before including GLM headers
+// // NOTE: GLSL uses radians, so will do the same, for consistency
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+glm::vec3 bgColour = glm::vec3(1.0, 0.0, 0.0);
+
+SDL_Window *win = nullptr;
+SDL_GLContext glcontext;
+
+bool initSDL(void);
+bool initOGL(void);
+void GameLoop(void);
+void update(void);
+void cleanUp(void);
